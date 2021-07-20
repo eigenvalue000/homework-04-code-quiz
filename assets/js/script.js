f0d2dbac10371bfe182f7a7b6c2d05fb6e3407eb
Garrett Kegel <ghkegel@gmail.com>
2021-07-20 16:05:44 -0700

    Created a startGame() function which uses a for loop to display all questions and their corresponding answers in the console. Strategy is to play the game in the console, then figure out how to make a gui for the game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 var questionsArray;
 var answersArray;
+var startButton = document.querySelector('.start-button');
 
 // Questions and answers obtained from tutorialspoint.com
 // Answers: D, A, B, B, A, D, D, A, B, C
 questionsArray = ["Which of the following is an advantage of using JavaScript?",
                   "Which of the following type of variable is visible everywhere in your JavaScript code?",
                   "Which built-in method combines the text of two strings and returns a new string?",
-                  "Which of the following code creates an object?"],
+                  "Which of the following code creates an object?",
                   "Which of the following function of Number object returns a string value version of the current number?",
                   "Which of the following function of String object returns the characters in a string between two indexes into the string?",
                   "Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag?",
                   "Which of the following functions of the Array object returns a new array comprised of this array joined with other array(s) and/or value(s)?",
                   "Which of the following function of Array object joins all elements of an array into a string?",
-                  "Which of the following function of Array object extracts a section of an array and returns a new array?";
+                  "Which of the following function of Array object extracts a section of an array and returns a new array?"];
 answersArray = [["Less server interaction", "Immediate feedback to visitors", 
                  "Increased interactivity", "All of the above"],
                 ["Gobal variable", "Local variable", "Both of the above", "None of the above"],["append()", "concat()", "attach()", "None of the above"],
@@ -24,5 +25,12 @@ answersArray = [["Less server interaction", "Immediate feedback to visitors",
                 ['concat()', 'join()', 'pop()', 'map()'],
                 ['reverse()', 'shift()', 'slice()', 'some()'] ];
 
+function startGame() {
+    for (i = 0; i < questionsArray.length; i++) {
+        console.log(questionsArray[i]);
+        console.log(answersArray[i]);
+    }
+    
+}
 
-
+startButton.addEventListener('click', startGame);
